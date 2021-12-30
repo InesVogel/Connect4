0e2798e5fca0cac6bbdefd09082724ae91eb53d0
Ines <ine.vogel@googlemail.com>
2021-12-30 20:15:46 +0100

    adds missing source code

---
+++ tests/test_agent_minimax_is_result_better.py
@@ -0,0 +1,73 @@
+import math
+
+from agents.agent_minimax import is_result_better
+from agents.agent_minimax.common import DEPTH
+from agents.common import PLAYER1, PLAYER2
+
+
+def test_is_result_better_PLAYER1_true():
+    best_score = -math.inf
+    best_num_moves = DEPTH
+    tmp_score = 1000
+    tmp_num_moves = DEPTH
+
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER1) == True
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER2) == False
+
+
+def test_is_result_better_PLAYER2_true():
+    best_score = math.inf
+    best_num_moves = DEPTH
+    tmp_score = -1000
+    tmp_num_moves = DEPTH
+
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER2) == True
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER1) == False
+
+
+def test_is_result_better_PLAYER1_tmpScoreLower_numMovesEqual_false():
+    best_score = 1000
+    best_num_moves = DEPTH
+    tmp_score = 999
+    tmp_num_moves = DEPTH
+
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER1) == False
+
+
+def test_is_result_better_PLAYER2_tmpScoreHigher_numMovesEqual_false():
+    best_score = 999
+    best_num_moves = DEPTH
+    tmp_score = 1000
+    tmp_num_moves = DEPTH
+
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER2) == False
+
+
+def test_is_result_better_scoresEqual_numMovesEqual_false():
+    best_score = 1000
+    best_num_moves = DEPTH
+    tmp_score = 1000
+    tmp_num_moves = DEPTH
+
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER1) == False
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER2) == False
+
+
+def test_is_result_better_scoresEqual_numMovesLower_true():
+    best_score = 1000
+    best_num_moves = 4
+    tmp_score = 1000
+    tmp_num_moves = 3
+
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER1) == True
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER2) == True
+
+
+def test_is_result_better_scoresEqual_numMovesHigher_false():
+    best_score = 1000
+    best_num_moves = 4
+    tmp_score = 1000
+    tmp_num_moves = 5
+
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER1) == False
+    assert is_result_better(best_score, best_num_moves, tmp_score, tmp_num_moves, PLAYER2) == False
